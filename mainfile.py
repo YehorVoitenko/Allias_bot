671efcd11751c8e5254ce643ffe498f4c3774d35
Yehor <egorka.voitenko@gmail.com>
2022-07-06 22:54:23 +0300

    Added code(by Yehor)

--- mainfile.py
+++ mainfile.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types= ['text'])
 def echo(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message((message.chat.id, message.text))
 
 # RUN
 bot.polling(non_stop = True)
