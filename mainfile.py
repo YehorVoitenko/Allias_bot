93c6d7c232b15692e854fa3d6a167e9268b91c0f
Yehor <egorka.voitenko@gmail.com>
2022-07-07 22:05:40 +0300

    Added two buttons(by Yehor)

--- mainfile.py
+++ mainfile.py
@@ -1,19 +1,42 @@
 import telebot
 import config
+from telebot import types
 
-bot = telebot.TeleBot(config.TOKEN)
+bot = telebot.TeleBot("5455678554:AAEqS1e20yR09YkRSC5GDtxvwFD37Gjd0_8")
 
-@bot.message_handler(commands = ['start'])
-def give_frog(message):
-    sti = open(r'E:\FirstTeleBot\Allias_bot\photos\frog.webp', 'rb')
-    bot.send_sticker(message.chat.id, sti)
-    bot.send_message(message.chat.id, "Чел, дарова блин, {0.first_name}! это я {1.first_name}. "
-                                     "Это так, просто учусь пока, ничё интересного". format(message.from_user, bot.get_me()),
-                     parse_mode='html')
 
-@bot.message_handler(content_types= ['text'])
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def greeting(message):
+    # count = 0
+    # bot.send_message(message.chat.id, 'hi, chose you team name:')
+    # bot.send_message(message.chat.id, count+1)
+    markup = types.ReplyKeyboardMarkup()
+    my_name = types.KeyboardButton('name')
+    sec_name = types.KeyboardButton('surname')
+    markup.add(my_name, sec_name)
+    bot.send_message(message.chat.id, 'сайт', reply_markup=markup)
+
+    # random = types.KeyboardButton('randon')
+
+
+    # if message.text == 'Выбрать случайное название':
+    #     bot.send_message(message.chat.id, 'count+1')
+
+
+# @bot.callback_query_handlers(func=lambda call: True)
+# def count1(call, message):
+#     count = 0
+#     if call.data == 'random':
+#         count += 1
+#         bot.send_message(message.chat.id, count)
+
+
+#
+# @bot.message_handler(content_types=['text'])
+# def echo(message):
+#     bot.send_message(message.chat.id, message.text)
 
 # RUN
+
+
 bot.polling(non_stop=True)
