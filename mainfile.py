d75ba11eb3f3b7524f8595a1a59b3a019039d8e3
Yehor <egorka.voitenko@gmail.com>
2022-07-09 00:01:01 +0300

    Work 'choose your own'(by Yehor)

--- mainfile.py
+++ mainfile.py
@@ -1,49 +1,30 @@
 import telebot
 from telebot import types
-import telegram
 
 bot = telebot.TeleBot("5455678554:AAEqS1e20yR09YkRSC5GDtxvwFD37Gjd0_8")
 
 
 @bot.message_handler(commands=['start'])
 def greeting(message):
-    markup_inline = types.InlineKeyboardMarkup()
-    first = types.InlineKeyboardButton(text='Yes', callback_data='yes')
-    sec = types.InlineKeyboardButton(text='No', callback_data='no')
-    markup_inline.add(first, sec)
-    bot.send_message(message.chat.id, 'Выбери вариант названия', reply_markup=markup_inline)
-# @bot.message_handler(commands=['start'])
-# def greeting(message):
-#     count = 0
-#     bot.send_message(message.chat.id, 'привет')
-#     markup = types.ReplyKeyboardMarkup()
-#     first = types.InlineKeyboardButton(text='name', callback_data='name')
-#     # sec = types.KeyboardButton('surname')
-#     markup.add(first) # , sec)
-#     bot.send_message(message.chat.id, 'сайт', reply_markup=markup)
+    markup_request = types.ReplyKeyboardMarkup()
+    markup_request.add(types.KeyboardButton('Своё название'), types.KeyboardButton('Случайное название'))
+    msg = bot.send_message(message.chat.id, "Выбери вариант название", reply_markup=markup_request)
+    bot.register_next_step_handler(msg, user_answer)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.data == 'yes':
-        bot.send_message(call.message.chat.id, 'Выбери')
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        first_id = types.KeyboardButton('первый ID')
-        sec_id = types.KeyboardButton('Имя')
-        markup_reply.add(first_id, sec_id)
-        bot.send_message(call.message.chat.id, 'Нажми на что-то справа снизу', reply_markup=markup_reply)
-    elif call.data == 'no':
-       pass
+def user_answer(message):
+    if message.text == 'Своё название':
+        msg1 = bot.send_message(message.chat.id, "Введите название")
+        bot.register_next_step_handler(msg1, user_put_name)
+    elif message.text == 'Случайное название':
+        bot.send_message(message.chat.id, "Вы выбрали случайное название")
 
 
-@bot.message_handler(content_types=['text'])
-def get_text(message):
-    if message.text == 'первый ID':
-        bot.send_message(message.chat.id, f'Yours id: {message.from_user.id}')
-    elif message.text == 'Имя':
-        bot.send_message(message.chat.id, f'Yours names: {message.from_user.first_name}')
-
+def user_put_name(message):
+    team_name = message.text
+    bot.send_message(message.chat.id, f'Your name {team_name}')
 
 
 
 bot.polling(non_stop=True)
+
