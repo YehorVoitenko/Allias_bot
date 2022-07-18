14b7a7d5e9ace5dd225a0530daa2f400e248f42a
Yehor <egorka.voitenko@gmail.com>
2022-07-18 09:20:16 +0300

    All features added(by Yehor)

--- mainfile.py
+++ mainfile.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 import telebot
 import random
@@ -8,14 +7,10 @@ bot = telebot.TeleBot("5455678554:AAEqS1e20yR09YkRSC5GDtxvwFD37Gjd0_8")
 team_number = 1
 first_team_name = None
 second_team_name = None
-
-
-@bot.message_handler(commands=['teaminfo'])
-def give_name(message):
-    global first_team_name
-    global second_team_name
-    bot.send_message(message.chat.id, f'<b>First team name: {first_team_name}</b>\n'
-                                      f'<b>Second team name: {second_team_name}</b>', parse_mode='html')
+first_team_points = 0
+second_team_points = 0
+turn = 2
+rounds = 0
 
 
 @bot.message_handler(commands=['start'])
@@ -50,7 +45,6 @@ def choose_name_way(message):
             greeting(message)
 
 
-
 def random_name():
     with open('first name.txt', 'r') as file:  # Instead of copying funcs - use class or another file; decorators
         content = file.read()
@@ -79,38 +73,59 @@ def own_team_name(message):
         greeting(message)
 
 
-turn = 2
-
-
 def are_you_ready(message):
+    global first_team_name
+    global second_team_name
+    global first_team_points
+    global second_team_points
     global turn
-    if turn == 2:
-        turn = 1
+    global rounds
+    if turn == second_team_name:  # Create class for check
+        turn = first_team_name
     else:
-        turn = 2
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,  one_time_keyboard=True)
+        turn = second_team_name
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     start = types.InlineKeyboardButton('Готов')
     markup.add(start)
-    msg = bot.send_message(message.chat.id, f"Команда {turn} готова?", reply_markup=markup, parse_mode='html')
-    bot.register_next_step_handler(msg, timer)
+    if first_team_points != 0 or second_team_points != 0:
+        show_points(message)
+    if rounds == 2:
+        game_over(message)
+    else:
+        msg = bot.send_message(message.chat.id, f"Команда <b><u>{turn}</u></b> готова?", reply_markup=markup,
+                               parse_mode='html')
+        bot.register_next_step_handler(msg, timer)
+        rounds += 1
 
 
 def show_word(message):
     with open('words.txt', 'r') as file:  # Instead of copying funcs - use class or another file; decorators
-            content = file.read()
-            splited = content.split(", ")
-            word = random.choice(splited)
+        content = file.read()
+        splited = content.split(", ")
+        word = random.choice(splited)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    markup.add(types.InlineKeyboardButton(text='+', callback_data='plus'), types.InlineKeyboardButton(text='-', callback_data='minus'))
+    markup.add(types.InlineKeyboardButton(text='+', callback_data='plus'),
+               types.InlineKeyboardButton(text='-', callback_data='minus'))
     msg = bot.send_message(message.chat.id, f'<b>{word}</b>', reply_markup=markup, parse_mode='html')
     bot.register_next_step_handler(msg, plus_or_minus)
 
 
 def plus_or_minus(message):
-    if message.text == '':
+    global turn
+    global first_team_points
+    global second_team_points
+    if message.text == '+':
+        if turn == second_team_name:
+            second_team_points += 1
+        else:
+            first_team_points += 1
         bot.send_message(message.chat.id, '+1 балл')
         show_word(message)
     elif message.text == '-':
+        if turn == second_team_name:
+            second_team_points -= 1
+        else:
+            first_team_points -= 1
         bot.send_message(message.chat.id, '-1 балл')
         show_word(message)
 
@@ -122,5 +137,30 @@ def timer(message):
     are_you_ready(message)
 
 
-bot.polling(non_stop=True)
+def show_points(message):
+    global first_team_points
+    global second_team_points
+    global first_team_points
+    global second_team_points
+    bot.send_message(message.chat.id, f"<b>Баллы команды {first_team_name}: {first_team_points}</b>\n<b>"
+                                      f"Баллы команды {second_team_name}: {second_team_points}</b>", parse_mode='html')
 
+
+def game_over(message):
+    global first_team_name
+    global second_team_name
+    global first_team_points
+    global second_team_points
+    markup = types.ReplyKeyboardRemove()
+    if first_team_points > second_team_points:
+        winner = first_team_name
+    elif first_team_points < second_team_points:
+        winner = second_team_name
+    elif first_team_points == second_team_points:
+        winner = "Ничья"
+
+    bot.send_message(message.chat.id, f"Игра окончена, спасибо за игру)\n"
+                                      f"Победитель: {winner}", reply_markup=markup)
+
+
+bot.polling(non_stop=True)
