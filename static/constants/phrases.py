f9731a9d034b9e7284d920d7e79120a1768753c6
Yehor <egorka.voitenko@gmail.com>
2022-07-19 14:54:19 +0300

    File with strings, using at project

--- static/constants/phrases.py
+++ static/constants/phrases.py
@@ -1,2 +1,7 @@
+from static.constants import variables
 own_name = 'Своё название'
 random_name = 'Случайное название'
+thanks = 'Игра окончена, спасибо за игру)'
+team_points = 'Баллы команды'
+next_turn = 'Ход переходит следующей команде'
+x_team_name = f'Название {variables.team_number}-ой команды'
