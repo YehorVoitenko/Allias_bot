0f7bdee551716c4f5641b68bc4770bb837e0eea5
Yehor <egorka.voitenko@gmail.com>
2022-07-19 14:51:26 +0300

    Replaced modules to other dirs

--- startapp.py
+++ startapp.py
@@ -1,21 +1,20 @@
-from Allias_bot.settings import config
+from settings import config
 import time
-import random
 import telebot
 from telebot import types
-# from modules.counter import compare
-from Allias_bot.static.constants import variables, phrases
-from modules.work_with_files import output_random_name, show_word
+from modules.counter import compare, check
+from static.constants import variables, phrases
+from modules.work_with_files import output_random_name, print_word
+from static.objects import buttons
+from modules.notifications import who_win
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(commands=['start'])
-def greeting(message):
+def start(message):
     if variables.team_number <= 2:
-        markup_request = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        markup_request.add(types.InlineKeyboardButton(phrases.own_name),
-                           types.InlineKeyboardButton(phrases.random_name))
+        markup_request = buttons.choose_name()
         msg = bot.send_message(message.chat.id, f'{variables.team_number}-ая выбери вариант названия',
                                reply_markup=markup_request)
         bot.register_next_step_handler(msg, choose_name_way)
@@ -24,44 +23,32 @@ def greeting(message):
 
 
 def choose_name_way(message):
-
     if message.text == 'Своё название':
         msg = bot.send_message(message.chat.id, "Введите название")
         bot.register_next_step_handler(msg, own_team_name)
     elif message.text == 'Случайное название':
         name = output_random_name()
-        bot.send_message(message.chat.id, f"Название {variables.team_number}-ой команды: "
-                                          f"<b><u>{output_random_name()}</u></b>\n",
-                                          parse_mode='html')
-        if variables.team_number == 1:
-            variables.first_team_name = name
-        if variables.team_number == 2:
-            variables.second_team_name = name
-        if variables.team_number <= 2:
-            variables.team_number += 1
-            greeting(message)
+        bot.send_message(message.chat.id, f"{phrases.x_team_name}: "
+                                          f"<b><u>{output_random_name()}</u></b>\n", parse_mode='html')
+        compare(name)
+        start(message)
 
 
 def own_team_name(message):
-    bot.send_message(message.chat.id, f"Название {variables.team_number}-ой команды: <b><u>{message.text}</u></b>",
+    bot.send_message(message.chat.id, f"{phrases.x_team_name}: <b><u>{message.text}</u></b>",
                      parse_mode='html')
-    if variables.team_number == 1:
-        variables.first_team_name = message.text
-    if variables.team_number == 2:
-        variables.second_team_name = message.text
-    if variables.team_number <= 2:
-        variables.team_number += 1
-        greeting(message)
+    compare(message.text)
+    start(message)
 
 
 def are_you_ready(message):
-    if variables.turn == variables.second_team_name:  # Create class for check
+    if variables.turn == variables.second_team_name:
         variables.turn = variables.first_team_name
     else:
         variables.turn = variables.second_team_name
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    start = types.InlineKeyboardButton('Готов')
-    markup.add(start)
+    ready = types.InlineKeyboardButton('Готов')
+    markup.add(ready)
     if variables.first_team_points != 0 or variables.second_team_points != 0:
         show_points(message)
     if variables.rounds == 2:
@@ -74,58 +61,35 @@ def are_you_ready(message):
 
 
 def show_word(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    markup.add(types.InlineKeyboardButton('+'),
-               types.InlineKeyboardButton('-'))
-    with open('static/words.txt', 'r') as file:  # Instead of copying funcs - use class or another file; decorators
-        content = file.read()
-        splited = content.split("\n")
-        word = random.choice(splited)
-    msg = bot.send_message(message.chat.id, f'<b>{word}</b>', reply_markup=markup, parse_mode='html')
-    bot.register_next_step_handler(msg, plus_or_minus)
+    markup_request = buttons.plus_minus()
+    msg = bot.send_message(message.chat.id, f'<b>{print_word()}</b>', reply_markup=markup_request, parse_mode='html')
+    bot.register_next_step_handler(msg, plus_minus_check)
 
 
-def plus_or_minus(message):
-    if message.text == '+':
-        if variables.turn == variables.second_team_name:
-            variables.second_team_points += 1
-        else:
-            variables.first_team_points += 1
-        bot.send_message(message.chat.id, '+1 балл')
-        show_word(message)
-    elif message.text == '-':
-        if variables.turn == variables.second_team_name:
-            variables.second_team_points -= 1
-        else:
-            variables.first_team_points -= 1
-        bot.send_message(message.chat.id, '-1 балл')
-        show_word(message)
+def plus_minus_check(message):
+    check(message.text)
+    show_word(message)
 
 
 def timer(message):
     show_word(message)
     time.sleep(5)
-    bot.send_message(message.chat.id, "<b>Ход переходит следующей команде</b>", parse_mode='html')
+    bot.send_message(message.chat.id, f"<b>{phrases.next_turn}</b>", parse_mode='html')
     are_you_ready(message)
 
 
 def show_points(message):
-    bot.send_message(message.chat.id, f"<b>Баллы команды {variables.first_team_name}: {variables.first_team_points}</b>\n<b>"
-                                      f"Баллы команды {variables.second_team_name}: {variables.second_team_points}</b>",
+    bot.send_message(message.chat.id, f"<b>{phrases.team_points} {variables.first_team_name}: "
+                                      f"{variables.first_team_points}</b>\n<b>"
+                                      f"{phrases.team_points} {variables.second_team_name}: "
+                                      f"{variables.second_team_points}</b>",
                      parse_mode='html')
 
 
 def game_over(message):
     markup = types.ReplyKeyboardRemove()
-    if variables.first_team_points > variables.second_team_points:
-        winner = variables.first_team_name
-    elif variables.first_team_points < variables.second_team_points:
-        winner = variables.second_team_name
-    elif variables.first_team_points == variables.second_team_points:
-        winner = "Ничья"
-
-    bot.send_message(message.chat.id, f"Игра окончена, спасибо за игру)\n"
-                                      f"Победитель: {winner}", reply_markup=markup)
+    bot.send_message(message.chat.id, f"{phrases.thanks}\n"
+                                      f"{who_win()}", reply_markup=markup, parse_mode='html')
 
 
 bot.polling(non_stop=True)
