e6f139d246a9a452940ee773c9a940df299e34e8
Yehor <egorka.voitenko@gmail.com>
2022-07-29 13:49:09 +0300

    Added DB retreives for first and second teams

--- repositories/retreives.py
+++ repositories/retreives.py
@@ -1,14 +1,22 @@
 from typing import List
 
-from utils.db_utils.teams import Team
+from utils.db_utils.teams import FirstTeam, SecondTeam
 from repositories.base import Session
 
 session = Session()
 
 
-def get_teams():
-    teams: List[Team] = session \
-        .query(Team) \
+def get_first_team():
+    teams: List[FirstTeam] = session \
+        .query(FirstTeam) \
+        .all()
+
+    return teams
+
+
+def get_second_team():
+    teams: List[SecondTeam] = session \
+        .query(SecondTeam) \
         .all()
 
     return teams
