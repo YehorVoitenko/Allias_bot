d51754f7701e9851a2596970ff93bea6960c8fdc
Yehor <egorka.voitenko@gmail.com>
2022-07-29 13:50:42 +0300

    Added fun for DB "not to check same thread"

--- repositories/base.py
+++ repositories/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///sqlite.db', echo=True, future=True)
+engine = create_engine('sqlite:///sqlite.db', echo=True, future=True, connect_args={'check_same_thread': False})
 
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
