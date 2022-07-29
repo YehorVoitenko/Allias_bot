44fd4c04cd11394e49d126545f11294cec420556
Yehor <egorka.voitenko@gmail.com>
2022-07-29 13:48:11 +0300

    Added DB for first and second teams

--- utils/db_utils/teams.py
+++ utils/db_utils/teams.py
@@ -2,12 +2,22 @@ from sqlalchemy import Integer
 from sqlalchemy import Column
 from sqlalchemy import String
 
-from constants.tables import TEAM_TABLE
+from constants.tables import FIRST_TEAM_TABLE, SECOND_TEAM_TABLE
 from repositories.base import Base as Base
 
 
-class Team(Base):
-    __tablename__ = TEAM_TABLE
+class FirstTeam(Base):
+    __tablename__ = FIRST_TEAM_TABLE
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+
+    def __init__(self, name=None):
+        self.name = name
+
+
+class SecondTeam(Base):
+    __tablename__ = SECOND_TEAM_TABLE
 
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
