d1c673991d88f4726fe6ca5fc3d2279e25575fa3
Yehor <egorka.voitenko@gmail.com>
2022-07-29 13:52:42 +0300

    Added DB for first and second teams

--- repositories/inserts.py
+++ repositories/inserts.py
@@ -1,10 +1,16 @@
-from utils.db_utils.teams import Team
+from utils.db_utils.teams import FirstTeam, SecondTeam
 from repositories.base import Session
 
 session = Session()
 
 
-def save_team(name: str):
-    t = Team(name=name)
+def add_first_team_info(name: str):
+    t = FirstTeam(name=name)
+    session.add(t)
+    session.commit()
+
+
+def add_second_team_info(name: str):
+    t = SecondTeam(name=name)
     session.add(t)
     session.commit()
